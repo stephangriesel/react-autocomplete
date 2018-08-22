284a57b81beda7058ccf7a0bdc244c073a38407e
Stephan Griesel <sgriesel@gmail.com>
2018-08-22 06:53:32 +0200

    start my autocomplete app

---
+++ src/Components/Autocomplete.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Autocomplete extends Component {
+  render() {
+    return (
+      <div className="Autocomplete">
+       My Autocomplete App
+      </div>
+    );
+  }
+}
+
+export default Autocomplete;
